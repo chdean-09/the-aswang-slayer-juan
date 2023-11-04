c0ba27408caae0027c85f16ff911cb2665ec1d14
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-04 23:07:57 +0800

    added logic to prevent holy meter value > 100

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -11,12 +11,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	current_bar_value -= BAR_SPEED * delta
+	
 	# Don't go below zero
 	current_bar_value = max(current_bar_value, 0)
-		
-	# Assuming this is a node you have set up
+	
 	$HolyMeter/TextureProgressBar.value = current_bar_value
 	
 func update_holy_meter():
 	current_bar_value += 3
+	
+	# Don't go above max value
+	current_bar_value = min(current_bar_value, 100)
+	
 	$HolyMeter/TextureProgressBar.value = current_bar_value
