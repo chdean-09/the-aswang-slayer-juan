68398d36de0101a243b29ceb2115c95f77fca2ef
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-04 18:03:55 +0800

    added holy meter
    pls tell me it works

---
+++ scenes/ui.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+const BAR_SPEED = 1
+var current_bar_value = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	current_bar_value -= BAR_SPEED * delta
+	# Don't go below zero
+	current_bar_value = max(current_bar_value, 0)
+	
+	# Assuming this is a node you have set up
+	$HolyMeter/TextureProgressBar.value = current_bar_value
