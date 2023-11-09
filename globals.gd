9fc4af56bd1db3cc960845958a659850534832b6
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-10 06:03:28 +0800

    added autoload of globals

---
+++ globals.gd
@@ -0,0 +1,13 @@
+extends Node
+
+signal health_change
+
+var health = 100:
+	get:
+		return health
+	set(value):
+		if value > health:
+			health = min(value, 100)
+		else:
+			health = value
+		health_change.emit()
--- scenes/holy_point.gd
+++ scenes/holy_point.gd
@@ -2,4 +2,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	body.add_item()
+	Globals.health -= 50
 	queue_free()
