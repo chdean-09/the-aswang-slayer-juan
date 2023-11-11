b7269e89ac7a42e9fef5a8d49c97e99524f08c50
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-12 04:23:26 +0800

    update enemy spawn

--- Level.gd
+++ Level.gd
@@ -13,11 +13,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# original world coordinate, your value
-	var pos = Globals.player_position
+#	var pos = Globals.player_position
 	# map coordinate
-	var cell_pos = tilemap.map_to_local(pos)
+#	var cell_pos = tilemap.map_to_local(pos)
 	# the Tile ID associated with that cell
-	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
+#	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
 	# the name of the Tile stored within the TileMap's TileSet resource
 	if timer.time_left > 80:
 		if spawnTimer.is_stopped():
@@ -37,27 +37,10 @@ func _process(_delta):
 func _on_player_update_stats():
 	$UI.update_holy_meter()
 
-#func _on_timer_timeout():
-#	var enemy = Enemy.instantiate()
-#
-#	enemy.position = Vector2(Globals.player_position) + Vector2(randf_range(-100, 100),randf_range(-100, 100))
-#
-#	var cell_pos = tilemap.map_to_local(enemy.position)
-#
-#	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
-#
-##	var tile_name = tilemap.tile_set.get_source(cell_tile_id)
-#
-#	print(cell_pos, cell_tile_id)
-#	if cell_tile_id == -1: 
-#		add_child(enemy)
-#	else: 
-#		print("collision")
-
 func _on_spawn_timer_timeout():
 	var enemy = Enemy.instantiate()
 	
-	enemy.position = Vector2(Globals.player_position) + Vector2(randf_range(-100, 100),randf_range(-100, 100))
+	enemy.position = Vector2(Globals.player_position) + Vector2(randf_range(-150, 150),randf_range(-150, 150))
 	
 	var cell_pos = tilemap.map_to_local(enemy.position)
 
