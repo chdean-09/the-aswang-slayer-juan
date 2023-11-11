b866a3dbe0aa0c5056ee014306c1a5a739fc1d1f
cezro <hyderz1218@gmail.com>
2023-11-11 22:38:46 +0800

    Work in Progress, hotbar works, w/o sprites for weapons, not working animations

--- player/player.gd
+++ player/player.gd
@@ -3,17 +3,24 @@ extends CharacterBody2D
 signal update_stats
 
 @export var plr_speed =  0 
-var acceleration = 50
-var min_plr_speed = 40
-var max_plr_speed = 100 
+@onready var acceleration = 50
+@onready var min_plr_speed = 40
+@onready var max_plr_speed = 100 
+@onready var spear_timer = $SpearAttackTimer
+@onready var arnis_timer = $ArnisAttackTimer
+@export var weapons = {"Arnis": "arnis", "Spear": "spear"}
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var current_weapon = weapons["Spear"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("bar_1"):
+		current_weapon = weapons["Spear"]
+		$AnimationPlayer.play("spear_idle")
+	elif Input.is_action_just_pressed("bar_2"):
+		current_weapon = weapons["Arnis"]
+		$AnimationPlayer.play("arnis_idle")
+	
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if direction == Vector2.ZERO:
@@ -29,11 +36,19 @@ func _process(delta):
 	rotate(get_angle_to(get_global_mouse_position()))
 	
 	if Input.is_action_pressed("lmb"):
-		$AnimationPlayer.play("arnis_attack")
-		$SpearAttackTimer.start()
-		
+		if current_weapon == weapons["Spear"] and spear_timer.is_stopped():
+			print('spear atk')
+			$AnimationPlayer.play("spear_attack")
+			spear_timer.start()
+			_on_spear_attack_timer_timeout()
+		elif current_weapon == weapons["Arnis"] and arnis_timer.is_stopped():
+			print('arnis atk')
+			$AnimationPlayer.play("arnis_attack")
+			arnis_timer.start()
+			_on_arnis_attack_timer_timeout()
+
 	if Input.is_action_pressed("rmb"):
-		print("right mouse button")
+		print(current_weapon)
 		
 		
 
@@ -41,4 +56,9 @@ func add_item() -> void:
 	update_stats.emit()
 
 func _on_spear_attack_timer_timeout():
+	$AnimationPlayer.play('spear_idle')
+	spear_timer.stop()
+
+func _on_arnis_attack_timer_timeout():
 	$AnimationPlayer.play('arnis_idle')
+	arnis_timer.stop()
