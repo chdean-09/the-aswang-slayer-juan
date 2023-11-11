6ef3079317b157e6a52dec90f7b3f816f2ef2e19
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2023-11-12 03:22:09 +0800

    added increasing spawn rate depending on game duration

--- Level.gd
+++ Level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var Enemy = preload("res://enemy/enemy.tscn")
 @onready var tilemap = $TileMap
+@onready var timer = $Timer
+@onready var spawnTimer = $SpawnTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,14 +19,42 @@ func _process(_delta):
 	# the Tile ID associated with that cell
 	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
 	# the name of the Tile stored within the TileMap's TileSet resource
-#	var tile_name = tilemap.tileset.tile_get_name(cell_tile_id)
-#	print(cell_pos, "pos")
-#	print(cell_tile_id, "id")
+	if timer.time_left > 80:
+		if spawnTimer.is_stopped():
+			print("100 to 80")
+			spawnTimer.start(5)
+	elif timer.time_left > 40:
+		if spawnTimer.is_stopped():
+			print("80 to 40")
+			spawnTimer.start(3)
+	elif timer.time_left > 0:
+		if spawnTimer.is_stopped():
+			print("40 to 0")
+			spawnTimer.start(1)
+	else:
+		print("boss")
 
 func _on_player_update_stats():
 	$UI.update_holy_meter()
 
-func _on_timer_timeout():
+#func _on_timer_timeout():
+#	var enemy = Enemy.instantiate()
+#
+#	enemy.position = Vector2(Globals.player_position) + Vector2(randf_range(-100, 100),randf_range(-100, 100))
+#
+#	var cell_pos = tilemap.map_to_local(enemy.position)
+#
+#	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
+#
+##	var tile_name = tilemap.tile_set.get_source(cell_tile_id)
+#
+#	print(cell_pos, cell_tile_id)
+#	if cell_tile_id == -1: 
+#		add_child(enemy)
+#	else: 
+#		print("collision")
+
+func _on_spawn_timer_timeout():
 	var enemy = Enemy.instantiate()
 	
 	enemy.position = Vector2(Globals.player_position) + Vector2(randf_range(-100, 100),randf_range(-100, 100))
@@ -33,8 +63,8 @@ func _on_timer_timeout():
 
 	var cell_tile_id = tilemap.get_cell_source_id(0, cell_pos)
 	
+#	var tile_name = tilemap.tile_set.get_source(cell_tile_id)
+	
 	print(cell_pos, cell_tile_id)
 	if cell_tile_id == -1: 
 		add_child(enemy)
-	else: 
-		print("collision")
